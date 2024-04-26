7c235ff881473971188fffe70697c19a1b07a22e
Natalia <nagonal2004@gmail.com>
2024-04-26 23:59:24 +0200

    gitflow-feature-stash: main
    envio de cambio en el main por fallo

--- Cine-Alba-Natalia/src/main/kotlin/Main.kt
+++ Cine-Alba-Natalia/src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
 package org.example
 
+import org.example.productos.models.butacas.repositories.ButacaRepositoryImpl
 import org.example.sala.services.SalaServiceImpl
 
 fun main() {
-    val salaService : SalaServiceImpl = SalaServiceImpl()
+    val salaService = SalaServiceImpl(butacaRepositoryImpl = ButacaRepositoryImpl())
 
     salaService.simular()
 }
